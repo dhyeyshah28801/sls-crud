646ebd9bcb610eaf7a7e175fb5b55d936a4d9f0b
Dhyey <dhyeyshah28801@gmail.com>
2023-05-12 08:30:54 +0530

    Initial Commit

---
+++ src/handlers/handler.js
@@ -0,0 +1,12 @@
+'use strict';
+ var Model = require("../../models/index")
+
+module.exports.addUser = function(event, context){
+console.log(event)
+    //   Model.User.create({
+//     firstName:event.body.firstName,      
+//     lastName:event.body.lastName,
+//     age:event.body.age
+//   });
+  return context;
+};
