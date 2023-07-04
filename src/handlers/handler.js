810126f8656b90a4e228b238a0a136476dfd5fc2
Dhyey Shah <dhyey.shah@wickes.co.uk>
2023-07-04 07:36:33 +0530

    second commit

--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -3,10 +3,10 @@
 
 module.exports.addUser = function(event, context){
 console.log(event)
-    //   Model.User.create({
-//     firstName:event.body.firstName,      
-//     lastName:event.body.lastName,
-//     age:event.body.age
-//   });
+  Model.User.create({
+    firstName:event.body.firstName,      
+    lastName:event.body.lastName,
+    age:event.body.age
+  });
   return context;
 };
